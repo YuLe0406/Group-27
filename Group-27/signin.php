7bc1acdddca5dbcdc1bddbedea4916dd17a8080b
TAN YU LE <1221207303@soffice.mmu.edu.my>
2024-06-26 23:39:24 +0800

    forgotpassword done!!!!!!

--- Group-27/signin.php
+++ Group-27/signin.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="sample.css">
-    <title>Andev Web</title>
+    <title>pepe sport shop</title>
 </head>
 
 <body>
