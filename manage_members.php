e11c86ffad5af6b2ae5d9f58d3f889f7eb04aaeb
Ricky Ng Long Kiat <1221205969@student.mmu.edu.my>
2024-06-26 15:11:35 +0800

    change

--- manage_members.php
+++ manage_members.php
@@ -33,12 +33,7 @@
                 <th>Password</th>
             </tr>
             <?php
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $dbname = "pepe_sportshop";
-
-            $conn = new mysqli($servername, $username, $password, $dbname);
+            $conn= mysqli_connect("localhost","root","","pepe_sportshop");
 
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
