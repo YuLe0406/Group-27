f0f06b70d82b305f22c60c2b4e30b12deb649e8d
TAN YU LE <1221207303@soffice.mmu.edu.my>
2024-06-30 16:30:59 +0800

    .

--- Group-27/signin.php
+++ Group-27/signin.php
@@ -77,7 +77,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div class="form-container sign-up">
             <form action="signin.php" method="POST">
                 <h1>Create Account</h1>
-                <input type="text" name="username" placeholder="Username" required="All fields are required!">
+                <input type="text" name="username" placeholder="Username" required>
                 <input type="email" name="email" placeholder="Email" required>
                 <input type="password" name="password" placeholder="Password" required>
                 <button type="submit" name="sign_up">Sign Up</button>
